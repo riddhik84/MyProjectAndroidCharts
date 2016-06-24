c0ce3f15937862661fe55aa7220bf06a4b2b0d27
riddhik84 <riddhik84@gmail.com>
2016-06-24 14:37:22 +0530

    BarChart Implementation using MPAndroidChart library

---
+++ app/src/main/java/com/riddhik/myapps/myprojectandroidcharts/MainActivityFragment.java
@@ -0,0 +1,39 @@
+package com.riddhik.myapps.myprojectandroidcharts;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_main, container, false);
+        Button MPAndroidChart = (Button) rootview.findViewById(R.id.mpandroidchart);
+
+        MPAndroidChart.setOnClickListener(this);
+        return rootview;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.mpandroidchart: {
+                Intent MPAndroidChartIntent = new Intent(getActivity(), MPAndroidChartActivity.class);
+                startActivity(MPAndroidChartIntent);
+            }
+        }
+
+    }
+}
